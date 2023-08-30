80b2684a53f234025fc0bea8ffe90a73bb44df35
qll1sk7 <142396437+qll1sk7@users.noreply.github.com>
2023-08-30 10:44:42 +0300

    Add files via upload

---
+++ bebra.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.forms import ModelForm
+
+TITLE_CHOICES = [
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+]
+
+
+ValidationError(_('Invalid value: %s') % value)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ['name', 'title', 'birth_date']
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'authors']
+
